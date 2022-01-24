66d30a58b399af0b4598239654039a41b5d8d61d
TaeUk-An <dksxodnr234@google.com>
2022-01-24 18:01:08 +0900

    Modify banner

--- cal_main.c
+++ cal_main.c
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
 	int a = atoi(argv[1]);
 	int b = atoi(argv[2]);
 
+	
+	printf("----------------------------------");
+	printf("-----------Calculate--------------");
 	printf("%d + %d = %d\n", a, b, add(a, b));
 	printf("%d - %d = %d\n", a, b, sub(a, b));
 	printf("%d * %d = %d\n", a, b, mul(a, b));
